5f3030c52fe586ea6476ef27f58af9674696f47e
NHQ SERVER ADMIN <nhq@NHQ.att.net>
2014-03-22 13:57:28 -0700

    standard maintenance

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = function (context, fn, bufSize) {
-    
+
     if (typeof context === 'function') {
-	fn = context;
-	context = new webkitAudioContext() ;
+      fn = context;
+      context = new webkitAudioContext() ;
     }
 
     if(!bufSize) bufSize = 4096;
@@ -10,79 +10,83 @@ module.exports = function (context, fn, bufSize) {
     var self = context.createScriptProcessor(bufSize, 1, 1);
 
     self.fn = fn
-    
+
     self.i = self.t = 0
-    
+
     window._SAMPLERATE = self.sampleRate = self.rate = context.sampleRate;
 
     self.duration = Infinity;
-    
+
     self.recording = false;
 
     self.onaudioprocess = function(e){
-	var output = e.outputBuffer.getChannelData(0)
-	,   input = e.inputBuffer.getChannelData(0);
-	self.tick(output, input);
+      var output = e.outputBuffer.getChannelData(0)
+      ,   input = e.inputBuffer.getChannelData(0);
+      self.tick(output, input);
     };
+
+    self._input = []
     
     self.tick = function (output, input) { // a fill-a-buffer function
 
-	output = output || self._buffer;
-	
-	input = input || []
+      output = output || self._buffer;
 
-	for (var i = 0; i < output.length; i += 1) {
+      input = input || self._input
 
-	    self.t = self.i / self.rate;
-	    
-	    self.i += 1;
+      for (var i = 0; i < output.length; i += 1) {
 
-	    output[i] = self.fn(self.t, self.i, input[i]);
-	    
-	    if(self.i >= self.duration) {
-		self.stop()
-		break;
-	    }
+          self.t = self.i / self.rate;
 
-	}
+          self.i += 1;
 
-	return output
+          output[i] = self.fn(self.t, self.i, input);
+
+          if(self.i >= self.duration) {
+            self.stop()
+            break;
+          }
+
+      }
+
+      return output
+      
     };
-    
+
     self.stop = function(){
-	self.disconnect();
-	
-	self.playing = false;
-	
-	if(self.recording) {
-  	}
+    
+      self.disconnect();
+
+      self.playing = false;
+
+      if(self.recording) {}
     };
 
     self.play = function(opts){
 
-	if (self.playing) return;
+      if (self.playing) return;
 
-	self.connect(self.context.destination);
-	
-	self.playing = true;
+      self.connect(self.context.destination);
 
-	return
+      self.playing = true;
+
+      return
+    
     };
 
     self.record = function(){
 
     };
-    
+
     self.reset = function(){
-	self.i = self.t = 0
+      self.i = self.t = 0
     };
-    
+
     self.createSample = function(duration){
-	self.reset();
-	var buffer = self.context.createBuffer(1, duration, self.context.sampleRate)
-	var blob = buffer.getChannelData(0);
-	self.tick(blob);
-	return buffer
+      self.reset();
+      var buffer = self.context.createBuffer(1, duration, self.context.sampleRate)
+      var blob = buffer.getChannelData(0);
+      self.tick(blob);
+      return buffer
     };
 
     return self;
@@ -92,7 +96,7 @@ function mergeArgs (opts, args) {
     Object.keys(opts || {}).forEach(function (key) {
         args[key] = opts[key];
     });
-    
+
     return Object.keys(args).reduce(function (acc, key) {
         var dash = key.length === 1 ? '-' : '--';
         return acc.concat(dash + key, args[key]);
